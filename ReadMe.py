d6f2719f6b919a085ad897233ad122b1bf292bba
nikkav79 <75850232+nikkav79@users.noreply.github.com>
2021-06-19 16:19:45 +0300

    Update ReadMe.py

--- ReadMe.py
+++ ReadMe.py
@@ -1,8 +1,16 @@
-#14.06.2021
-# 1. Разделение разработки моделей:
+# Работа с Git
+# Основная ветка проекта - main
+# Рабочая ветка - develop
+# Ветки разработчиков - dev_<имя разработчика>, например dev_nikkav79
+# Коммиты разработчиков должны быть максимально подробными для того, что бы другие разработчики могли оценить характер
+# изменений
+# Из веток разработчиков dev_<имя разработчика> данные переносятся в develop.
+# после проверки кода в develop информация переносится в main
+
+
+# 14.06.2021 -20.06.2021
+# 1. Разработка  models.py
 # Таньчик:
-# Staff
-# Costs
 # ContactDetails
 # SocialNetworks#
 #
@@ -15,11 +23,37 @@
 # Lessons
 # Media
 # Reviews
+# Flow (News)
 #
 # Миша:
 # Specialization
 # LessonType
+# Staff
+# Costs
 #
+#
+# 20.06.21-23.06.21
+# 2. Заполнение данных БД
+# Таньчик:
+# ContactDetails
+# SocialNetworks#
+#
+# Николай:
+# StudioDescription
+# Status
+# Vacancy
+# AgeGroups
+# AgeLessons (Не уверен что она нужна)
+# Lessons
+# Media
+# Reviews
+# Flow (News)
+#
+# Миша:
+# Specialization
+# LessonType
+# Staff
+# Costs
 
 
 
