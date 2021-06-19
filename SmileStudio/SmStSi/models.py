d33b31e5276f24d7036ea995ce01e031a54802f1
tritium <tinotiano@gmail.com>
2021-06-19 18:23:27 +0300

    create two models: ContactDetail, SocialNetworks

--- SmileStudio/SmStSi/models.py
+++ SmileStudio/SmStSi/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from phone_field import PhoneField
 
 
 
@@ -83,9 +84,33 @@ class Reviews(models.Model):
 
 class ContactDetails(models.Model):
     """Контактные данные студии"""
-    pass
+    city = models.CharField(max_length=100, verbose_name='Город')
+    district = models.CharField(max_length=150, verbose_name='Район')
+    street = models.CharField(max_length=150, verbose_name='Улица')
+    building = models.CharField(max_length=10, verbose_name='Дом')
+    postcode = models.PositiveBigIntegerField(verbose_name='Индекс')
+    phone = PhoneField(blank=True, help_text='Contact phone number', verbose_name='Телефон')
+    email = models.EmailField(max_length=254, blank=True, verbose_name='Электронная почта')
+    location = models.SlugField(verbose_name='Расположение на карте', blank=True)
+
+    def __str__(self):
+        return f'{self.city}, {self.street}, {self.building}'
+
+    class Meta:
+        verbose_name = 'Контактные данные'
+        verbose_name_plural = 'Контактные данные'
+        ordering = ['city']
 
 
 class SocialNetworks(models.Model):
     """Ссылки на социальные сети"""
-    pass
+    name = models.CharField(max_length=100, blank=True, verbose_name='Название')
+    link = models.SlugField(max_length=254, blank=True, verbose_name='Ссылка')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Социальная сеть'
+        verbose_name_plural = 'Социальные сети'
+        ordering = ['name']
