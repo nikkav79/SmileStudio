55015b7468336b756167241edd5f490230659ee5
lockorsa <lockorsa@yandex.ru>
2021-06-30 14:11:24 +0700

    Added model verbose names

--- SmileStudio/SmStSi/models.py
+++ SmileStudio/SmStSi/models.py
@@ -5,11 +5,11 @@ from django.contrib.auth.models import User
 
 class StudioDescription(models.Model):
     """Описание студии"""
-    about = models.TextField('О нас')
-    philosophy = models.TextField('Философия центра')
-    mission = models.TextField('Наша миссия')
-    sight = models.TextField('Наш взгляд')
-    target = models.TextField('Наша цель')
+    about = models.TextField(verbose_name='О нас')
+    philosophy = models.TextField(verbose_name='Философия центра')
+    mission = models.TextField(verbose_name='Наша миссия')
+    sight = models.TextField(verbose_name='Наш взгляд')
+    target = models.TextField(verbose_name='Наша цель')
 
     class Meta:
         verbose_name = 'О Нас'
@@ -48,9 +48,9 @@ class Staff(models.Model):
     last_name = models.CharField(max_length=100, verbose_name='Фамилия')
     experience = models.CharField(max_length=150, verbose_name='Опыт')
     education = models.CharField(max_length=150, verbose_name='Образование')
-    specialization = models.ForeignKey(Specialization, on_delete=models.CASCADE)
-    status = models.ForeignKey(Status, on_delete=models.CASCADE)
-    ad_information = models.TextField()
+    specialization = models.ForeignKey(Specialization, on_delete=models.CASCADE, verbose_name='Специализация')
+    status = models.ForeignKey(Status, on_delete=models.CASCADE, verbose_name='Статус')
+    ad_information = models.TextField(verbose_name='ad_information')
     is_active = models.BooleanField(default=True, verbose_name='Актив')
 
     def __str__(self):
@@ -64,8 +64,8 @@ class Staff(models.Model):
 ###
 class Vacancy(models.Model):
     """Вакансии студии"""
-    specialization = models.ForeignKey(Specialization, on_delete=models.CASCADE)
-    status = models.ForeignKey(Status, on_delete=models.CASCADE)
+    specialization = models.ForeignKey(Specialization, on_delete=models.CASCADE, verbose_name='Специализация')
+    status = models.ForeignKey(Status, on_delete=models.CASCADE, verbose_name='Статус')
     description = models.TextField(verbose_name='Описание')
     responsibilities = models.TextField(verbose_name='Обязанности')
     requirements = models.TextField(verbose_name='Требования')
@@ -105,12 +105,11 @@ class LessonsType(models.Model):
         verbose_name_plural = 'Типы уроков'
 
 
-
 class Costs(models.Model):
     """Стоимость занятий"""
-    lesson_type = models.ForeignKey(LessonsType, on_delete=models.CASCADE)
-    staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
-    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    lesson_type = models.ForeignKey(LessonsType, on_delete=models.CASCADE, verbose_name='Тип занятия')
+    staff = models.ForeignKey(Staff, on_delete=models.CASCADE, verbose_name='Сотрудник')
+    cost = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
 
     class Meta:
         verbose_name = 'Стоимость'
@@ -119,12 +118,12 @@ class Costs(models.Model):
 
 class Lessons(models.Model):
     """Занятия"""
-    lesson_type = models.ForeignKey(LessonsType, on_delete=models.CASCADE)
-    cost = models.ForeignKey(Costs, on_delete=models.CASCADE)
+    lesson_type = models.ForeignKey(LessonsType, on_delete=models.CASCADE, verbose_name='')
+    cost = models.ForeignKey(Costs, on_delete=models.CASCADE, verbose_name='Цена')
     duration = models.CharField(max_length=50, verbose_name='Длительность')
     days = models.CharField(max_length=100, verbose_name='Дни')
     hours = models.CharField(max_length=100, verbose_name='Часы')
-    lesson_age = models.ManyToManyField(AgeGroups)
+    lesson_age = models.ManyToManyField(AgeGroups, verbose_name='Возрастная группа')
     is_active = models.BooleanField(default=True, verbose_name='Актив')
 
     class Meta:
@@ -134,11 +133,11 @@ class Lessons(models.Model):
 
 class Media(models.Model):
     """Фото и видеоматериалы, их краткое описание"""
-    lesson = models.ForeignKey(Lessons, on_delete=models.CASCADE)
-    staff = models.ManyToManyField(Staff)
+    lesson = models.ForeignKey(Lessons, on_delete=models.CASCADE, verbose_name='Занятие')
+    staff = models.ManyToManyField(Staff, verbose_name='Сотрудник')
     date = models.DateTimeField(auto_now_add=True, verbose_name='Дата')
-    photo_link = models.ImageField(upload_to='post_files', blank=False, null=False)
-    video_file = models.FileField(upload_to='post_files', blank=True, null=True)
+    photo_link = models.ImageField(upload_to='post_files', blank=False, null=False, verbose_name='Ссылка на фото')
+    video_file = models.FileField(upload_to='post_files', blank=True, null=True, verbose_name='Ссылка на видео')
 
     class Meta:
         verbose_name = 'Медиа'
@@ -148,11 +147,11 @@ class Media(models.Model):
 ###
 class Reviews(models.Model):
     """Отзывы о преподавателях и студии"""
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    lesson = models.ForeignKey(Lessons, on_delete=models.CASCADE)
-    staff = models.ManyToManyField(Staff)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    lesson = models.ForeignKey(Lessons, on_delete=models.CASCADE, verbose_name='Занятие')
+    staff = models.ManyToManyField(Staff, verbose_name='Сотрудник')
     date = models.DateTimeField(auto_now_add=True, verbose_name='Дата')
-    message = models.TextField()
+    message = models.TextField(verbose_name='Текст отзыва')
     star_choice_1 = models.CharField(max_length=5, verbose_name='Рейтинг1')
     star_choice_2 = models.CharField(max_length=5, verbose_name='Рейтинг2')
     star_choice_3 = models.CharField(max_length=5, verbose_name='Рейтинг3')
@@ -172,7 +171,7 @@ class ContactDetails(models.Model):
     postcode = models.PositiveBigIntegerField(verbose_name='Индекс')
     phone = PhoneField(blank=True, help_text='Contact phone number', verbose_name='Телефон')
     email = models.EmailField(max_length=254, blank=True, verbose_name='Электронная почта')
-    location = models.CharField(max_length=150, verbose_name='Расположение на карте', blank=True)
+    location = models.CharField(max_length=150, verbose_name='Расположение на карте', blank=True, null=True)
 
     def __str__(self):
         return f'{self.city}, {self.street}, {self.building}'
@@ -187,7 +186,6 @@ class SocialNetworks(models.Model):
     """Ссылки на социальные сети"""
     name = models.CharField(max_length=100, blank=True, verbose_name='Название')
     link = models.CharField(max_length=254, blank=True, verbose_name='Ссылка')
-
     # link_1 = models.SlugField(max_length=254, blank=True, verbose_name='Ссылка')
 
     def __str__(self):
@@ -199,9 +197,8 @@ class SocialNetworks(models.Model):
         ordering = ['name']
 
 
-class NewsFlow(models.Model):
+class News(models.Model):
     """ Новости портала """
-
     created = models.DateTimeField(auto_now=True, verbose_name='Дата создания')
     modified = models.DateTimeField(auto_now_add=True, verbose_name='Дата изменения')
     title = models.CharField(max_length=150, verbose_name='Заголовок')
