d5d1a66e0fe1c53fff44158cd6b2117496e7ab7e
tritiumTino <75369981+tritiumTino@users.noreply.github.com>
2021-07-10 16:10:14 +0300

    Update models.py

--- SmileStudio/SmStSi/models.py
+++ SmileStudio/SmStSi/models.py
@@ -46,7 +46,7 @@ class ContractType(models.Model):
 class Media(models.Model):
     """Фото и видеоматериалы, их краткое описание"""
     date = models.DateTimeField(auto_now_add=True, verbose_name='Дата')
-    media_link = models.ImageField(upload_to='post_files', blank=False, null=False, verbose_name='Ссылка на медиа-файл')
+    media_link = models.FileField(upload_to='post_files', blank=False, null=False, verbose_name='Ссылка на медиа-файл')
     short_description = models.CharField(max_length=127, verbose_name='Краткое описание', blank=True, null=True)
     is_active = models.BooleanField(default=False, verbose_name='Выводится в галерею')
 
