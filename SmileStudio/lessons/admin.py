b95a29960c1d224d322a7eca06ac37c30ae34b3f
lockorsa <lockorsa@yandex.ru>
2021-07-20 23:04:31 +0700

    Последняя корректировка

--- SmileStudio/lessons/admin.py
+++ SmileStudio/lessons/admin.py
@@ -2,20 +2,19 @@ from django.contrib import admin
 from .models import *
 
 
-"""class TimeTableInline(admin.TabularInline):
-    model = TimeTable"""
+class TimeTableInline(admin.TabularInline):
+    model = TimeTable
 
 
-"""class LessonsAdmin(admin.ModelAdmin):
+class LessonsAdmin(admin.ModelAdmin):
     inlines = [
         TimeTableInline,
     ]
-    exclude = ('days',)"""
+    exclude = ('days',)
 
 
 admin.site.register(AgeGroups)
 admin.site.register(LessonsType)
 admin.site.register(Costs)
 admin.site.register(WeekDays)
-admin.site.register(Lessons, ) # LessonsAdmin
-admin.site.register(TimeTable, ) # TimeTableInline
+admin.site.register(Lessons, LessonsAdmin)
--- SmileStudio/mainpage/admin.py
+++ SmileStudio/mainpage/admin.py
@@ -1,8 +1,1 @@
 from django.contrib import admin
-# from about.admin import *
-# from lessons.models import *
-# from media.models import *
-# from news.models import *
-# from reviews.models import *
-# from team.models import *
-
