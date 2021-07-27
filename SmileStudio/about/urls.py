e29343893d977b795a20efc108cab56cc8ca76aa
tritium <tinotiano@gmail.com>
2021-07-27 22:20:20 +0300

    подготовка проекта для написания API

--- SmileStudio/about/urls.py
+++ SmileStudio/about/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('about/', ContactsListView.as_view(), name='contacts_cbv_url'),
+    path('', ContactsListView.as_view(), name='contacts_cbv_url'),
 ]
